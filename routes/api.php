afecc985e9bdf363a12ad2e65f0fe06d89b4b490
DhikaID <bhadrika.223040018@mail.unpas.ac.id>
2024-08-11 20:56:56 +0700

    feat: user profile all

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Resources\GetResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,5 +12,10 @@ Route::post('/user/signup', [AuthController::class, 'signup'])->middleware('gues
 Route::post('/user/signin', [AuthController::class, 'signin'])->middleware('guest')->name('login');
 Route::post('/user/signout', [AuthController::class, 'signout'])->middleware('auth:sanctum');
 Route::get('/user/me', [AuthController::class, 'user'])->middleware('auth:sanctum');
+Route::put('/user/edit', [AuthController::class, 'edit'])->middleware('auth:sanctum');
 
-//
+
+// DEV
+Route::get('/user/all', function () {
+    return new GetResource(200, 'Sukses mengambil data.', User::all());
+});
